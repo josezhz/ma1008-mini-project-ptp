4d68aba0f3de8519a11bc8b6961111b477e10752
josezhz <zhang.haozhe@trentglobal.edu.sg>
2023-04-11 15:19:50 +0800

    Enabled drawing polygons and its hatch lines with holes, combined drawPolygon() and drawHatchlines() into one function

--- main.py
+++ main.py
@@ -16,33 +16,39 @@ def init():
   t.goto(x_max, y_max)
   t.goto(x_min, y_max)
   t.goto(x_min, y_min)
-init()
 
-vertices = [(50, 20), (70, 30), (80, 50), (70, 90), (70, 50), (50, 50), (30, 90), (20, 50), (30, 31)]
-# vertices = []
-# inputMode = int(sc.numinput("Input Mode", "1: File input\n2: Interactive input (under dev.)", 1, 1, 1))
-# if inputMode == 1:
-#   while True:
-#     vertexNo = len(vertices) + 1
-#     x = float(sc.numinput(f"Vertex No.{vertexNo}", (("Existing vertices:\n" + "".join([str(v) + "\n" for v in vertices])) if len(vertices) else "") + "x = ", minval=0, maxval=100))
-#     y = float(sc.numinput(f"Vertex No.{vertexNo}", (("Existing vertices:\n" + "".join([str(v) + "\n" for v in vertices])) if len(vertices) else "") + "y = ", minval=0, maxval=100))
-#     vertices.append((x, y))
-#     if vertexNo >= 3:
-#       addVertex = int(sc.numinput("Add another vertex?", "1: Yes\n0: No", minval=0, maxval=1))
-#       if not addVertex:
-#         break
-def drawPolygon(vertices):
-  t.penup()
-  t.goto(vertices[-1])
-  t.pendown()
-  for vertex in vertices:
-    t.goto(vertex)
-drawPolygon(vertices)
+def getPolygonData():
+  vertices = []
+  while True:
+    vertexNo = len(vertices) + 1
+    x = float(sc.numinput(f"Vertex No.{vertexNo}", (("Existing vertices:\n" + "".join([str(v) + "\n" for v in vertices])) if len(vertices) else "") + f"x{vertexNo} = ", minval=0, maxval=100))
+    y = float(sc.numinput(f"Vertex No.{vertexNo}", (("Existing vertices:\n" + "".join([str(v) + "\n" for v in vertices])) if len(vertices) else "") + f"y{vertexNo} = ", minval=0, maxval=100))
+    vertices.append((x, y))
+    if vertexNo >= 3:
+      addVertex = int(sc.numinput("Add vertex?", "1: Yes\n0: No", minval=0, maxval=1))
+      if not addVertex:
+        break
+  holes = []
+  while True:
+    holeVertices = []
+    while True:
+      holeVertexNo = len(holeVertices) + 1
+      x = float(sc.numinput(f"Hole Vertex No.{holeVertexNo}", (("Existing hole vertices:\n" + "".join([str(v) + "\n" for v in holeVertices])) if len(holeVertices) else "") + f"x{holeVertexNo} = ", minval=0, maxval=100))
+      y = float(sc.numinput(f"Hole Vertex No.{holeVertexNo}", (("Existing hole vertices:\n" + "".join([str(v) + "\n" for v in holeVertices])) if len(holeVertices) else "") + f"y{holeVertexNo} = ", minval=0, maxval=100))
+      holeVertices.append((x, y))
+      if holeVertexNo >= 3:
+        addHoleVertex = int(sc.numinput("Add hole vertex?", "1: Yes\n0: No", minval=0, maxval=1))
+        if not addHoleVertex:
+          holes.append(holeVertices)
+          break
+    addHole = int(sc.numinput("Add hole?", "1: Yes\n0: No", minval=0, maxval=1))
+    if not addHole:
+      break
+  return {"vertices": vertices, "holes": holes}
 
-hatchLineData = {"angle": 30, "spacing": 5, "color": "red"}
 def getHatchLineData():
   angle = float(sc.numinput("Hatch Line Angle", "unit: degree", minval=-89.99999999, maxval=89.99999999))
-  spacing = float(sc.numinput("Hatch Line Spacing", "", minval=0))
+  spacing = float(sc.numinput("Hatch Line Spacing", "Hatch line spacing =", minval=0))
   colorMode = int(sc.numinput("Hatch Line Color Mode", "1: text (eg. red)\n2: RGB (eg. (255, 0, 0))\n3: HEX (eg. #ff0000)"))
   if colorMode == 2:
     r = int(sc.numinput("Hatch Line Color", "R =", minval=0, maxval=255))
@@ -51,39 +57,58 @@ def getHatchLineData():
     color = (r, g, b)
   else:
     color = sc.textinput("Hatch Line Color", "eg. " + ("red" if colorMode == 1 else "#ff0000"))
-# getHatchLineData()
-def drawHatchLines(hatchLineData):
+  return {"angle": angle, "spacing": spacing, "color": color}
+        
+def draw(polygonData, hatchLineData):
+  # Draw Polygon
+  polygons = [polygonData["vertices"]] + [i for i in polygonData["holes"]]
+  for p in polygons:
+    t.penup()
+    t.goto(p[-1])
+    t.pendown()
+    for v in p:
+      t.goto(v)
+  # Draw Hatch Line
   [angle, spacing, color] = hatchLineData.values()
   angle = angle * math.pi / 180
+  if angle == 0:
+    angle = 0.000000001
   numOfHatchLines = math.floor(100 * math.sqrt(2) * math.sin(abs(angle) + math.pi / 4) / spacing) # Calculate the number of hatch lines the screen can contain
-  for i in range(- numOfHatchLines, numOfHatchLines + 1):
-    start_hatch = (0, i * (spacing / math.cos(angle)))
-    direction_hatch = (1, math.tan(angle))
+  for i in range(- numOfHatchLines, 1):
+    start_hatch = (100 if angle > 0 else 0, - i * (spacing / math.cos(angle))) # Determine A1
+    direction_hatch = (1, math.tan(angle)) # Determine V1
     intersections = []
-    for i in range(len(vertices)):
-      start_edge = vertices[i]
-      nextIndex = i + 1 if i < len(vertices) - 1 else 0
-      direction_edge = (vertices[nextIndex][0] - vertices[i][0], vertices[nextIndex][1] - vertices[i][1])
-      A1x, A1y = start_hatch
-      V1x, V1y = direction_hatch
-      A2x, A2y = start_edge
-      V2x, V2y = direction_edge
-      t1 = ((A2x*V2y - A2y*V2x) - (A1x*V2y - A1y*V2x)) / (V1x*V2y - V1y*V2x) # t value for the hatch line equation
-      t2 = ((A2x*V1y - A2y*V1x) - (A1x*V1y - A1y*V1x)) / (V1x*V2y - V1y*V2x) # t value for the edge equation
-      if 0 <= t2 <= 1: # If the intersection lies within the edge
-        intersection = (A1x + V1x * t1, A1y + V1y*t1)
-        intersections.append(intersection)
-    def getIndex0(x): return x[0]
-    intersections.sort(key=getIndex0)
+    for p in polygons:
+      for i in range(len(p)):
+        start_edge = p[i] # Determine A2
+        nextIndex = i + 1 if i < len(p) - 1 else 0
+        direction_edge = (p[nextIndex][0] - p[i][0], p[nextIndex][1] - p[i][1]) # Determine V2
+        A1x, A1y = start_hatch
+        V1x, V1y = direction_hatch
+        A2x, A2y = start_edge
+        V2x, V2y = direction_edge
+        t2 = ((A2x*V1y - A2y*V1x) - (A1x*V1y - A1y*V1x)) / (V1x*V2y - V1y*V2x) # t value for the edge equation
+        if 0 <= t2 < 1: # If the intersection lies within the edge
+          intersection = (A2x + V2x * t2, A2y + V2y*t2)
+          intersections.append(intersection)
+    intersections.sort()
     if len(intersections):
-      for i in range(len(intersections)):
-        if i % 2 == 0:
-          t.penup()
-          t.goto(intersections[i])
-          t.pendown()
-          t.pencolor(color)
-          t.goto(intersections[i + 1])
-drawHatchLines(hatchLineData)
+      for i in range(0, len(intersections) - 1, 2):
+        t.penup()
+        t.goto(intersections[i])
+        t.pendown()
+        t.pencolor(color)
+        t.goto(intersections[i + 1])
+
+init()
+polygonData = {
+  "vertices": [(50, 20), (70, 30), (80, 50), (70, 90), (50, 50), (30, 90), (20, 50), (30, 30)],
+  "holes": [[(25, 50), (45, 50), (30, 80)], [(30, 40), (70, 40), (50, 25)]]
+}
+# polygonData = getPolygonData()
+hatchLineData = {"angle": 30, "spacing": 1, "color": "red"}
+# hatchLineData = getHatchLineData()
+draw(polygonData, hatchLineData)
 
 sc.exitonclick()
 t.done()
